5a589c0fca44217e0d48156d4418df6d9c73c521
marinagosson <marinagosson@snowmanlabs.com>
2026-06-11 21:03:27 -0300

    feat: add api validation and swagger documentation

---
+++ backend/api/src/docs/swagger.ts
@@ -0,0 +1,37 @@
+export const swaggerDocument = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Mobile Engineering Playground API',
+    version: '1.0.0',
+  },
+  paths: {
+    '/auth/login': {
+      post: {
+        summary: 'Authenticate user',
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  email: {
+                    type: 'string',
+                  },
+                  password: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          '200': {
+            description: 'Login success',
+          },
+        },
+      },
+    },
+  },
+};
